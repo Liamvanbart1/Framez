d04dbc67a9a1f9721c649343f71e4dbda2e9a65b
QuyNg13 <quythieu34@gmail.com>
2025-06-10 14:20:39 +0200

    include ipv exclude

--- server/server.js
+++ server/server.js
@@ -16,33 +16,27 @@ app
 
 app.use(async (req, res, next) => {
   try {
-    // Pagina-ID's die je wilt uitsluiten uit het menu
-    const excludedPageIds = [
-      49503, 46253, 45363, 42363, 42391, 42441, 42429, 42387, 42339,
-      39909, 35525, 34841, 26211, 25435, 25413, 25323, 24671, 24665,
-      25247, 23687, 23247, 22171, 21397, 20873, 20769, 20743, 20247,
-      19967, 18555, 18553, 18551, 18549, 17573, 16866, 17081, 16742,
-      16735, 15249, 11491, 10877, 10865, 10859, 10851, 10735, 8563,
-      8013, 5495, 5483, 5419, 3955, 2668, 1482, 378, 258
+    const includedPageIds = [
+      //page-id's
     ];
 
     const response = await fetch('https://framerframed.nl/en/wp-json/wp/v2/pages?per_page=100');
     const allPages = await response.json();
 
-    // Filter 
-    const visiblePages = allPages.filter(page => !excludedPageIds.includes(page.id));
+    //pagina's in includedPageIds laten zien
+    const visiblePages = allPages.filter(page => includedPageIds.includes(page.id));
 
-    // schildren en parent pages definieren
+    // Child en parent pages definiëren
     const mainPages = visiblePages.filter(page => page.parent === 0);
     const subPages = visiblePages.filter(page => page.parent !== 0);
 
-    // children aan parent toevoegen
+    // Children aan parent toevoegen
     const menu = mainPages.map(parent => ({
       ...parent,
       children: subPages.filter(child => child.parent === parent.id),
     }));
 
-    // Zet menu beschikbaar voor alle views
+    // menu op alle views
     res.locals.menu = menu;
   } catch (err) {
     console.error('Fout bij ophalen van menu:', err);
@@ -58,7 +52,6 @@ app.get('/', async (req, res) => {
 });
 
 const renderTemplate = async (template, data = {}, res = null) => {
-  //menu toevoegen aan de data
   if (res && res.locals.menu) {
     data.menu = res.locals.menu;
   }
